5c4ad824d6e07adde73090bfb872d5cf6b939f0b
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2023-03-03 11:07:32 +0100

    repo: split repo according to its need of GH and local git repo

--- alpa/repository.py
+++ alpa/repository.py
@@ -1,12 +1,12 @@
 """
-Set of commands that helps with integration of Fedora Alternative Packaging
+Set of commands that helps with integration of Alternative Packaging
 repository.
 """
-
-
-from os import getcwd
+from os import getcwd, environ
 from pathlib import Path
 import re
+from subprocess import call
+from tempfile import NamedTemporaryFile
 from typing import List, Optional
 from urllib.parse import urlparse
 
@@ -14,35 +14,29 @@ from click import UsageError, ClickException
 import click
 from git import Repo, Remote
 from alpa.constants import (
-    FAP_FEAT_BRANCH,
-    FAP_FEAT_BRANCH_PREFIX,
+    ALPA_FEAT_BRANCH,
+    ALPA_FEAT_BRANCH_PREFIX,
     MAIN_BRANCH,
     ORIGIN_NAME,
     UPSTREAM_NAME,
     PackageRequest,
 )
-from alpa.github import GithubAPI
+from alpa.gh import GithubAPI, GithubRepo
 from alpa.messages import (
     CLONED_REPO_IS_NOT_FORK,
     NO_WRITE_ACCESS_ERR,
+    NOT_IN_PREDEFINED_STATE,
 )
 
 
-class FAPRepo:
-    def __init__(
-        self,
-        repo_path: Path,
-        gh_api: Optional[GithubAPI] = None,
-    ) -> None:
+class LocalRepo:
+    def __init__(self, repo_path: Path) -> None:
         self.repo_path = repo_path
-        self.local_repo = Repo(self.repo_path)
+        self.local_repo = Repo(str(self.repo_path))
         self.git_cmd = self.local_repo.git
 
-        self.gh_api = gh_api or GithubAPI()
-
-        repo_name = "repo_name"
-        namespace = "the_namespace"
-        self.gh_repo = self.gh_api.get_repo(namespace, repo_name)
+        if not self._is_repo_in_predefined_state():
+            raise ClickException(NOT_IN_PREDEFINED_STATE)
 
     @property
     def branch(self) -> str:
@@ -50,36 +44,48 @@ class FAPRepo:
 
     @property
     def package(self) -> str:
-        return self.branch.lstrip(FAP_FEAT_BRANCH_PREFIX)
+        return self.branch.lstrip(ALPA_FEAT_BRANCH_PREFIX)
 
     def get_packages(self, regex: str) -> List[str]:
         remote_refs = self.local_repo.remote(name=UPSTREAM_NAME).refs
         if regex == "":
-            return list(remote_refs)
+            return [pkg.name for pkg in remote_refs]
 
         pattern = re.compile(regex)
-        return [pkg for pkg in remote_refs if pattern.match(pkg)]
+        return [pkg.name for pkg in remote_refs if pattern.match(pkg)]
 
     def _is_repo_in_predefined_state(self) -> bool:
         remotes_name_set = {remote.name for remote in self.local_repo.remotes}
         return remotes_name_set == {ORIGIN_NAME, UPSTREAM_NAME}
 
-    def _prepare_cloned_repo(self) -> None:
-        Remote.create(self.local_repo, UPSTREAM_NAME, self.gh_repo.upstream_url)
+    def switch_to_package(self, package: str) -> None:
+        self.git_cmd.switch(package)
+
+    def get_history_of_branch(self, branch: str, *params: List[str]) -> None:
+        return self.git_cmd.log("--all", "--decorate", "--graph", *params, branch)
 
     @staticmethod
-    def clone(url: str) -> None:
-        # in case of `@` in url -> remove the `git@` prefix form it
-        repo_path = urlparse(url.split("@")[-1]).path
-        parsed_repo_path = repo_path.strip("/").strip(".git")
-        namespace, repo_name = parsed_repo_path.split("/")
-        api = GithubAPI()
-        gh_repo = api.get_repo(namespace, repo_name)
-        if not gh_repo.is_fork():
-            raise UsageError(CLONED_REPO_IS_NOT_FORK)
+    def _get_message_from_editor() -> str:
+        with NamedTemporaryFile(suffix=".alpa.tmp") as temp_file:
+            call([environ.get("EDITOR", "vim"), temp_file.name])
+            temp_file.seek(0)
+            output = temp_file.read()
+            if isinstance(output, (bytes, bytearray)):
+                return output.decode("utf-8")
 
-        cloned_repo = Repo.clone_from(url, getcwd())
-        FAPRepo(cloned_repo.working_dir, api)._prepare_cloned_repo()
+            return output
+
+    def commit(self, message: str) -> None:
+        if self.branch == self.package:
+            click.echo("Switching to feature branch")
+            self.git_cmd.switch("-c", ALPA_FEAT_BRANCH.format(pkgname=self.package))
+
+        index = self.local_repo.index
+        index.add("*")
+        if message:
+            index.commit(message)
+        else:
+            index.commit(self._get_message_from_editor())
 
     def pull(self, branch: str) -> None:
         self.git_cmd.pull(ORIGIN_NAME, branch)
@@ -87,17 +93,19 @@ class FAPRepo:
     def push(self, branch: str) -> None:
         self.git_cmd.push(ORIGIN_NAME, f"{self.branch}:{branch}")
 
-    def commit(self, message: str) -> None:
-        if self.branch == self.package:
-            click.echo("Switching to feature branch")
-            self.git_cmd.switch("-c", FAP_FEAT_BRANCH.format(pkgname=self.package))
+    def _get_full_reponame(self) -> str:
+        for remote in self.local_repo.remotes:
+            if remote.name == ORIGIN_NAME:
+                return remote.url.split(":")[-1].rstrip(".git")
 
-        index = self.local_repo.index
-        index.add("*")
-        if message:
-            index.commit(message)
-        else:
-            index.commit()
+
+class AlpaRepo(LocalRepo):
+    def __init__(self, repo_path: Path, gh_api: Optional[GithubAPI] = None) -> None:
+        super().__init__(repo_path)
+
+        self.gh_api = gh_api or GithubAPI()
+        namespace, repo_name = self._get_full_reponame().split("/")
+        self.gh_repo = self.gh_api.get_repo(namespace, repo_name)
 
     def create_package(self, package: str) -> None:
         upstream = self.gh_repo.get_upstream()
@@ -108,14 +116,8 @@ class FAPRepo:
         self.git_cmd.switch("-c", package)
         self.push(package)
 
-    def switch_to_package(self, package: str) -> None:
-        self.git_cmd.switch(package)
-
     def request_package(self, package_name: str) -> None:
-        upstream = self.gh_repo.get_upstream()
-        if upstream is None:
-            upstream = self.gh_repo
-
+        upstream = self.gh_repo.get_root_repo()
         upstream_namespace = upstream.namespace
         issue_repo = self.gh_api.get_repo(upstream_namespace, self.gh_repo.repo_name)
         issue = issue_repo.create_issue(
@@ -131,5 +133,25 @@ class FAPRepo:
     def delete_package(self) -> None:
         pass
 
-    def get_history_of_branch(self, branch: str, *params: List[str]) -> None:
-        return self.git_cmd.log("--all", "--decorate", "--graph", *params, branch)
+    @staticmethod
+    def _prepare_cloned_repo(local_repo: Repo, gh_repo: GithubRepo) -> None:
+        # tady je potreba to neudelat v self
+        Remote.create(local_repo, UPSTREAM_NAME, gh_repo.upstream_url)
+
+    @staticmethod
+    def _get_repo_name_from_url(repo_url: str) -> str:
+        return repo_url.split("/")[-1].rstrip(".git")
+
+    @classmethod
+    def clone(cls, url: str) -> None:
+        # in case of `@` in url -> remove the `git@` prefix form it
+        repo_path = urlparse(url.split("@")[-1]).path
+        parsed_repo_path = repo_path.strip("/").strip(".git")
+        namespace, repo_name = parsed_repo_path.split("/")
+        api = GithubAPI()
+        gh_repo = api.get_repo(namespace, repo_name)
+        if not gh_repo.is_fork():
+            raise UsageError(CLONED_REPO_IS_NOT_FORK)
+
+        cloned_repo = Repo.clone_from(url, getcwd() + cls._get_repo_name_from_url(url))
+        cls._prepare_cloned_repo(cloned_repo, gh_repo)
