4859f92fdda490f293b09bc37cf1e96bda72ac49
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2023-04-07 17:40:52 +0200

    fix: remove unused packit constant

--- alpa_conf/constants.py
+++ alpa_conf/constants.py
@@ -1,11 +1,2 @@
 METADATA_FILE_NAMES = [".metadata", "metadata"]
 METADATA_SUFFIXES = ["yaml", "yml"]
-
-PACKIT_CONFIG_NAMES = [
-    ".packit.yaml",
-    ".packit.yml",
-    ".packit.json",
-    "packit.yaml",
-    "packit.yml",
-    "packit.json",
-]
