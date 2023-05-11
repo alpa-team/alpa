c5e7d0e17b8c9c96831530e8dea7def5c164f2d0
Jiri Kyjovsky <j1.kyjovsky@gmail.com>
2023-05-11 22:23:43 +0200

    feat: return stdout and stderr from git cmd

--- alpa/git.py
+++ alpa/git.py
@@ -17,6 +17,21 @@ class GitCmdResult:
     stderr: str
     retval: int
 
+    @property
+    def stderr_and_stdout(self) -> str:
+        result = ""
+        if self.stdout:
+            result += self.stdout
+
+        if not self.stderr:
+            return result
+
+        if result:
+            result += "\n"
+
+        result += self.stderr
+        return result
+
 
 class GitCMD:
     def __init__(self, cwd: str) -> None:
